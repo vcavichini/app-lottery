f2c9550b8907b760ebdf13092d15da82755fb6a6
vcavichini <vcavichini@gmail.com>
2024-04-07 12:27:14 -0300

    bug fix 3

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const https = require('https')
-const express = require('express')
-const axios = require('axios')
+const https = require('https');
+const express = require('express');
+const axios = require('axios');
 
-const apiurl = "https://servicebus2.caixa.gov.br/portaldeloterias/api/megasena/"
-const port = process.env.PORT || 3001
+const apiurl = "https://servicebus2.caixa.gov.br/portaldeloterias/api/megasena/";
+const port = process.env.PORT || 3001;
 
 const meusjogos = [
   ["03","15","18","23","40","54"],
@@ -13,15 +13,11 @@ const meusjogos = [
   ["04","09","13","24","30","55"]
 ];
 
-// const httpsAgent = new https.Agent({
-//   rejectUnauthorized: false,
-// });
-
 const app = express()
-app.set('views', 'views')
-app.set('view engine', 'ejs')
-app.use(express.json()) // for parsing application/json bodies
-app.use(express.urlencoded({extended:true}))
+app.set('views', 'views');
+app.set('view engine', 'ejs');
+app.use(express.json()); // for parsing application/json bodies
+app.use(express.urlencoded({extended:true}));
 
 app.get('/:nr_conc?', async (req, res) => {
   let nr_conc = req.params.nr_conc ?? "";
@@ -29,11 +25,8 @@ app.get('/:nr_conc?', async (req, res) => {
 
   try {
     dt = await getData(endpoint);
-  } catch(err) {
-    console.log('ERROR ignored');
-  }
 
-  res.render('home', {
+    res.render('home', {
       concurso: dt.numero,
       listaDezenas: dt.listaDezenas,
       dataApuracao: dt.dataApuracao,
@@ -52,58 +45,20 @@ app.get('/:nr_conc?', async (req, res) => {
       },
       meusjogos: meusjogos,
     });
+  } catch(err) {
+    res.render('error');
+    console.log('ERROR ignored');
   }
-);
-
-  // axios.get(endpoint, { httpsAgent })
-  //   // Print data
-  //   .then( function (response) {
-  //     const dt = response.data;
-  //     res.render('home', {
-  //       concurso: dt.numero,
-  //       listaDezenas: dt.listaDezenas,
-  //       dataApuracao: dt.dataApuracao,
-  //       dataProximoConcurso: dt.dataProximoConcurso,
-  //       valorEstimadoProximoConcurso: formata_moeda(dt.valorEstimadoProximoConcurso),
-  //       lista_numeros: dt.listaDezenas,
-  //       concurso_anterior: dt.numeroConcursoAnterior,
-  //       concurso_proximo: dt.numeroConcursoProximo,
-  //       rateio: {
-  //         sena: formata_qtde(dt.listaRateioPremio[0].numeroDeGanhadores),
-  //         premioSena: formata_moeda(dt.listaRateioPremio[0].valorPremio),
-  //         quina: formata_qtde(dt.listaRateioPremio[1].numeroDeGanhadores),
-  //         premioQuina: formata_moeda(dt.listaRateioPremio[1].valorPremio),
-  //         quadra: formata_qtde(dt.listaRateioPremio[2].numeroDeGanhadores),
-  //         premioQuadra: formata_moeda(dt.listaRateioPremio[2].valorPremio),
-  //       },
-  //       meusjogos: meusjogos,
-  //     });
-  //   })
-  //   // Print error message if occur
-  //   .catch( function (error) {
-  //     // if (error.response) {
-  //     //   // The request was made and the server responded with a status code
-  //     //   // that falls out of the range of 2xx
-  //     //   console.log(error.response.data);
-  //     //   console.log(error.response.status);
-  //     //   console.log(error.response.headers);
-  //     // }
-  //     console.log(`Error to fetch data\n ${error}`);
-  //     res.render('error');
-  //   })
-  //   // .finally(function () {
-  //   //   console.log(`==> finally`);
-  //   // })
-  // });
+});
 
 function formata_moeda(valor) {
-  var dinheiro = valor.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })
-  return dinheiro
+  var dinheiro = valor.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
+  return dinheiro;
 }
 
 function formata_qtde(qtd) {
-  var q = qtd.toLocaleString('pt-br')
-  return q
+  var q = qtd.toLocaleString('pt-br');
+  return q;
 }
 
 async function getData(endpoint) {
@@ -113,4 +68,4 @@ async function getData(endpoint) {
   return jsonResponse;
 } 
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
